f957ecc9827ecca262bb7cf65cc9376de9947ce3
Myltiane Alves <myltiane.aux@gmail.com>
2021-11-13 13:08:14 -0300

    Adicionando frontend

--- pages/api/rooms/index.js
+++ pages/api/rooms/index.js
@@ -1,11 +1,19 @@
-import nc from 'next-connect';
-import dbConnect from '../../../config/dbConnect';
-import { allRoms } from '../../../controllers/roomControllers';
+import nc from 'next-connect'
+import dbConnect from '../../../config/dbConnect'
 
-const handler = nc();
+import { allRooms, newRoom } from '../../../controllers/roomControllers'
+
+import onError from '../../../middlewares/errors'
+import { isAuthenticatedUser, authorizeRoles } from '../../../middlewares/auth'
+
+const handler = nc({ onError });
 
 dbConnect();
 
-handler.get(allRoms)
+handler.get(allRooms)
+
+handler
+    .use(isAuthenticatedUser, authorizeRoles('admin'))
+    .post(newRoom)
 
 export default handler;
